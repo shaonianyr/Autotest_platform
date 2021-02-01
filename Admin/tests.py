c187b157f06410760df488a0ed3d0452f23ca6a9
shaonian <licetianyr@163.com>
2021-02-01 18:13:33 +0800

    项目初始化

--- Admin/tests.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-# Create your tests here.
-
-
-
-
-driver.get('http://www.baidu.com')
-print(driver.title)
--- Product/tests.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-
-
-webdriver.Chrome()
